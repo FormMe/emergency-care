8d9bde7c558710d0d3cf06cc31405eb7c1990cdf
Ilya Ovchinnikov <ilya.ovchinnikov@dasha.ai>
2022-05-17 17:31:17 +0300

    messages class

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import telebot
 from telebot import apihelper, types
 
 import config
+from messages import Messages
 
 apihelper.ENABLE_MIDDLEWARE = True
 bot = telebot.TeleBot(config.TOKEN)
 
-with open("data/meta.json") as f:
-    data_meta = json.load(f)
+messages = Messages("data/messages.json")
 
 sessions = dict()
 
@@ -43,37 +43,49 @@ def ask_problem(message):
     if message.chat.type == "private":
         if message.text == "Критическая ситуация":
             markup = types.InlineKeyboardMarkup()
-            for pet_id, pet_info in data_meta["pet_help"].items():
-                item = types.InlineKeyboardButton(text=pet_info["name"], callback_data=pet_id)
+            for pet_id, pet_emergency in messages.emergency.items():
+                item = types.InlineKeyboardButton(text=pet_emergency.name, callback_data=pet_emergency.id)
                 markup.add(item)
             bot.send_message(message.chat.id, "Какое у вас животное?", reply_markup=markup)
 
         if message.text == "Дополнительная информация":
             bot.send_message(message.chat.id, "Что вы хотите узнать?")
 
+def match_pet_query(pet_query):
+    return pet_query.data in messages.emergency
 
-@bot.callback_query_handler(func=lambda query: query.data in data_meta["pet_help"])
-def select_pet(query):
+@bot.callback_query_handler(func=match_pet_query)
+def select_pet(pet_query):
     print(bot.session)
-    bot.session["pet"] = query.data
+    bot.session["pet"] = pet_query.data
     markup = types.InlineKeyboardMarkup()
-    for problem_id, problem_info in data_meta["pet_help"][bot.session["pet"]]["problems"].items():
-        item = types.InlineKeyboardButton(text=problem_info["title"], callback_data=problem_id)
+    pet_id = bot.session["pet"]
+    for problem in messages.emergency[pet_id].problems:
+        item = types.InlineKeyboardButton(text=problem.title, callback_data=problem.id)
         markup.add(item)
-    bot.send_message(query.message.chat.id, f"хорошо. у вас {bot.session['pet']}. Что случилось?", reply_markup=markup)
+    pet_name = bot.session['pet']
+    bot.send_message(pet_query.message.chat.id, f"хорошо. у вас {pet_name}. Что случилось?", reply_markup=markup)
 
 
-@bot.callback_query_handler(
-    func=lambda query: bot.session["pet"] is not None
-    and query.data in data_meta["pet_help"][bot.session["pet"]]["problems"]
-)
+def match_problem_query(problem_query):
+    pet_name = bot.session["pet"]
+    pet_selected = pet_name is not None
+    match_problem = problem_query.data in messages.emergency[pet_name].problems
+    return pet_selected and match_problem
+
+@bot.callback_query_handler(func=match_problem_query)
 def select_problem(query):
     print(bot.session)
     bot.session["problem"] = query.data
-    filename = data_meta["pet_help"][bot.session["pet"]]["problems"][bot.session["problem"]]["text"]
-    with open(f"data/pet_help/{bot.session['pet']}/{filename}") as f:
-        solution = f.read()
-    bot.send_message(query.message.chat.id, solution)
+    selected_pet = bot.session["pet"]
+    selected_problem = bot.session["problem"]
+    problem = messages.emergency[selected_pet][selected_problem]
+    if problem is not None:
+        msg = problem.msg
+    else:
+        msg = "Problem not found"
+    
+    bot.send_message(query.message.chat.id, msg)
 
 
 bot.polling(none_stop=True)
